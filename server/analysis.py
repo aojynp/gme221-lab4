38580a85e2ae5a0de7595f4cd208a5adc9c39a5a
Aojyn Perez <aaperez5@up.edu.ph>
2026-03-23 13:14:57 +0800

    Spatial Autocorrelation

--- server/analysis.py
+++ server/analysis.py
@@ -2,6 +2,9 @@ import geopandas as gpd
 from sqlalchemy import create_engine
 from spatial_weights import contiguity_weights, knn_weights, distance_weights 
 from visualization import visualize_neighbors
+from moran import calculate_global_morans_I
+from esda.moran import Moran_Local
+import os
 
 host = "localhost" 
 port = "5433" 
@@ -38,3 +41,25 @@ w = contiguity_weights(gdf)
 
 #VISUALIZATION
 visualize_neighbors(gdf, w)
+
+#Moran Visualization
+attribute = "ass_ass_va" 
+moran_I, p_value = calculate_global_morans_I(gdf, w, attribute) 
+print("Global Moran's I:", moran_I)
+print("p-value:", p_value) 
+
+local = Moran_Local(gdf["ass_ass_va"], w) 
+gdf["local_I"] = local.Is 
+gdf["p_value"] = local.p_sim 
+
+gdf["cluster"] = "Not Significant" 
+gdf.loc[(gdf["local_I"] > 0) & (gdf["p_value"] < 0.05), "cluster"] = "Hotspot" 
+gdf.loc[(gdf["local_I"] < 0) & (gdf["p_value"] < 0.05), "cluster"] = "Coldspot" 
+
+os.makedirs("output", exist_ok=True) 
+gdf.to_file( 
+    "output/spatial_clusters.geojson", 
+    driver="GeoJSON" 
+) 
+print("Saved: output/spatial_clusters.geojson") 
+
